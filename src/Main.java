6fc99f6bd57f35aa0a8b970b2537396a5705db5e
sinjwu <harbirdouk@gmail.com>
2025-05-30 14:40:56 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 25;
+
+        System.out.println(age);
+
+        age = 26;
+
+        System.out.println(age);
+
+        double salary = 50000.75;
+
+        System.out.println(salary);
+
+        String name = "sinjwu";
+
+        System.out.println(name);
+
+        System.out.println(name.length());
+    }
+}
